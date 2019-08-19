eefe5001baf680c9ff0f281d6ee5f8dc61abbf48
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-08-19 15:02:59 -0600

    Set up more views along with View Controller files

---
+++ Trekker/Controller/View Controller/Home Tab/HikeDetailsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HikeDetailsViewController.swift
+//  Trekker
+//
+//  Created by Jason Mandozzi on 8/19/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class HikeDetailsViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pitcureCollectionView: UICollectionView!
+    @IBOutlet weak var directionsButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var hikeMapButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //Actions
+    @IBAction func directionsButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func hikeMapButtonTapped(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
