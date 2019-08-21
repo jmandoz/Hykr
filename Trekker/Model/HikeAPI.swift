9ece43b1597f8cdddc57d1ffef851a842c0d0430
Drew Seeholzer <drews21f7@gmail.com>
2019-08-21 13:56:55 -0600

    Changed hikeAPI model to reflect new api.

--- Trekker/Model/HikeAPI.swift
+++ Trekker/Model/HikeAPI.swift
@@ -9,42 +9,34 @@
 import Foundation
 
 // Primary hike JSON
+
+struct TopLevelJSON: Decodable {
+    
+    let trails: [HikeJSON]
+}
+
 struct HikeJSON: Decodable {
-    let coordinates: Coordinates
+    let longitude: Double
+    let latitude: Double
     let hikeName: String
     let apiID: Int
-    let hikeImages: [HikeImagesJSON?]
+    let hikeImageURLAsString: String
+    let hikeRating: Double
     
     enum CodingKeys: String, CodingKey {
-        case coordinates = "starting_trailhead_id"
+        case longitude
+        case latitude
         case hikeName = "name"
         case apiID = "id"
-        case hikeImages = "medium"
-    }
-}
-
-struct Coordinates: Decodable {
-    let latitude: Double
-    let longitude: Double
-}
-
-// Route coordinates JSON
-struct HikeRouteJSON: Decodable {
-    let route: String
-}
-
-// Hike images JSON
-struct HikeImagesJSON: Decodable {
-    let imageURLAsString: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case imageURLAsString = "medium"
+        case hikeImageURLAsString = "imgSmallMed"
+        case hikeRating = "stars"
     }
 }
 
 struct HikeAPIStrings {
-    static let baseURL = "https://api.transitandtrails.org/api/v1"
-    static let tripsComponent = "trips"
+    static let baseURL = "https://www.hikingproject.com/data/get-trails"
+    static let latitudeQuery = "lat"
+    static let longitudeQuery = "lon"
    static let apiKey = "key"
-   static let apiKeyValue = "2a4e49414c8400a3219a127e803e6f965684fdb6a777b0bf1dc7c5f3e2270093"
+   static let apiKeyValue = "200558723-bb50c93f3346f8a98fefed60f3a8b2dc"
 }
