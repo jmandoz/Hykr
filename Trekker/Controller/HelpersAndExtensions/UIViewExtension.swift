992fee099191365ed9e3e5fba165108e6ec9b619
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-08-21 21:02:45 -0600

    added the animated drawer view for the hike details

---
+++ Trekker/Controller/HelpersAndExtensions/UIViewExtension.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewExtension.swift
+//  Trekker
+//
+//  Created by Jason Mandozzi on 8/21/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, topPadding: CGFloat, bottomPadding: CGFloat, leadingPadding: CGFloat, trailingPadding: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: topPadding).isActive = true
+        }
+        
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: leadingPadding).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: bottomPadding).isActive = true
+        }
+        
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: trailingPadding).isActive = true
+        }
+        
+        if let height = height {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+        if let width = width {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+    }
+}
--- Trekker/Controller/View Controller/HomeTab/HomePageViewController.swift
+++ Trekker/Controller/View Controller/HomeTab/HomePageViewController.swift
@@ -11,6 +11,8 @@ import MapKit
 
 class HomePageViewController: UIViewController {
     
+    let screenSize = UIScreen.main.bounds.size
+    
     let currentLongitude = CoreLocationController.shared.locationManager.location?.coordinate.longitude
     let currentLatitude = CoreLocationController.shared.locationManager.location?.coordinate.latitude
     
@@ -23,11 +25,40 @@ class HomePageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         CoreLocationController.shared.activateLocationServices()
-        fetchHikes()
-        
-        
+        getMyRegion()
+        fetchHikes ()
+        detailView.alpha = 1
+        detailView.frame = CGRect(x: 0, y: self.screenSize.height, width: self.screenSize.width, height: self.screenSize.height / 3.5)
         // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        addSubViews()
+        setUpMainStackView()
+    }
+    
+    func getMyRegion() {
+        DispatchQueue.main.async {
+            guard let latitude = CoreLocationController.shared.locationManager.location?.coordinate.latitude, let longitude = CoreLocationController.shared.locationManager.location?.coordinate.longitude else {return}
+            let userCoordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let mySpan = MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
+            let myRegion = MKCoordinateRegion(center: userCoordinates, span: mySpan)
+            self.mapView.setRegion(myRegion, animated: true)
+        }
+    }
+    
+    func createAnnotations(hikeArray: [HikeJSON]) {
+        for hike in hikeArray {
+            DispatchQueue.main.async {
+                guard let long = hike.longitude, let lat = hike.latitude else {return}
+                let annotations = MKPointAnnotation()
+                annotations.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                self.mapView.addAnnotation(annotations)
+            }
+        }
     }
     
     func fetchHikes() {
@@ -38,21 +69,56 @@ class HomePageViewController: UIViewController {
         NetworkController.sharedInstance.getDataFromURL(url: url) { (data) in
             
             guard let data = data else { return }
-            
             do {
                 let decoder = JSONDecoder()
                 let topLevelJSON = try decoder.decode(TopLevelJSON.self, from: data)
                 guard let trails = topLevelJSON.trails else { return }
                 self.searchResults = trails
-                
             } catch {
                 print ("Error in \(#function) : \(error.localizedDescription) /n---/n \(error)")
                 return
             }
+            self.createAnnotations(hikeArray: self.searchResults)
         }
     }
     
-
+    //Hike Detail View
+    let detailView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalCentering
+        stackView.spacing = 15
+        return stackView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Helvetca", size: 14)
+        label.text = "Hike Name"
+        label.textColor = .black
+        return label
+    }()
+    
+    func addSubViews() {
+        self.view.addSubview(detailView)
+        self.view.addSubview(mainStackView)
+    }
+    
+    func setUpMainStackView() {
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.addArrangedSubview(nameLabel)
+    }
+    
+    func constrainView() {
+        mainStackView.anchor(top: detailView.topAnchor, bottom: detailView.bottomAnchor, leading: detailView.leadingAnchor, trailing: detailView.trailingAnchor, topPadding: 5, bottomPadding: -5, leadingPadding: 5, trailingPadding: -5)
+    }
 
     /*
     // MARK: - Navigation
@@ -64,4 +130,33 @@ class HomePageViewController: UIViewController {
     }
     */
 
+}
+
+extension HomePageViewController: MKMapViewDelegate {
+    
+
+    
+    func showDetailView() {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseIn, animations: {
+                self.detailView.alpha = 1
+                self.detailView.frame = CGRect(x: 0, y: self.screenSize.height - (self.screenSize.height/3), width: self.screenSize.width, height: self.screenSize.height / 3)
+            }, completion: nil)
+    }
+    
+    func hideDetailView() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
+            self.detailView.frame = CGRect(x: 0, y: self.screenSize.height, width: self.screenSize.width, height: 0)
+        }, completion: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        DispatchQueue.main.async {
+            self.showDetailView()
+        }
+    }
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        DispatchQueue.main.async {
+            self.hideDetailView()
+        }
+    }
 }
